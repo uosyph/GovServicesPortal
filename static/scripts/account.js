d8dfe3e9a755416d44d603efc52d0dbeef8472c8
Yousef Saeed <yousafesaeed@gmail.com>
2024-03-11 11:55:28 +0200

    [feat] Disable buttons when possible

--- static/scripts/account.js
+++ static/scripts/account.js
@@ -24,3 +24,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+const form = document.getElementById('updateForm');
+const inputs = form.querySelectorAll('input[type=text]');
+const submitButton = document.getElementById('submitButton');
+
+// Disable submit button on page load
+submitButton.disabled = true;
+
+inputs.forEach(input => {
+    input.addEventListener('input', function () {
+        let changed = false;
+        inputs.forEach(input => {
+            if (input.defaultValue !== input.value) changed = true;
+        });
+
+        if (changed) submitButton.disabled = false;
+        else submitButton.disabled = true;
+    });
+});
